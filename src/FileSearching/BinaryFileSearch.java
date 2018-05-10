b5f11c8735c63467593c13a978ad9134615a39a7
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-09 20:15:09 -0700

    Overhauled BinaryFileSearch Class
    complete overhaul on the BinaryFileSearch class. Changed to only a single method with recursive call to check two conditions and return a counter for files found under given name.

--- src/FileSearching/BinaryFileSearch.java
+++ src/FileSearching/BinaryFileSearch.java
@@ -12,87 +12,45 @@ package FileSearching;
 /*						PSEUDOCODE					*/
 /* class BinaryFileSearch{
  * 
- * String for file name
- * String for directory name
+ * 
  * 
  * }
  * */
 /****************************************************/
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BinaryFileSearch {
 
-  private String fileNameToSearch;
-  private List<String> result = new ArrayList<String>();
 	
-  public String getFileNameToSearch() {
-	return fileNameToSearch;
-  }
-
-  public void setFileNameToSearch(String fileNameToSearch) {
-	this.fileNameToSearch = fileNameToSearch;
-  }
-
-  public List<String> getResult() {
-	return result;
-  }
-
-  public static void main(String[] args) {
-
-	BinaryFileSearch binaryFileSearch = new BinaryFileSearch();
-  
-        //try different directory and filename :)
-	binaryFileSearch.searchDirectory(new File("/Users/Quigy Laptop/workspace/"), "shape.java");
-
-	int count = binaryFileSearch.getResult().size();
-	if(count ==0){
-	    System.out.println("\nNo result found!");
-	}else{
-	    System.out.println("\nFound " + count + " result!\n");
-	    for (String matched : binaryFileSearch.getResult()){
-		System.out.println("Found : " + matched);
-	    }
-	}
-  }
-
-  public void searchDirectory(File directory, String fileNameToSearch) {
-
-	setFileNameToSearch(fileNameToSearch);
-
-	if (directory.isDirectory()) {
-	    search(directory);
-	} else {
-	    System.out.println(directory.getAbsoluteFile() + " is not a directory!");
-	}
-
-  }
-
-  private void search(File file) {
-
-	if (file.isDirectory()) {
-	  System.out.println("Searching directory ... " + file.getAbsoluteFile());
+	public int searchForFile(String directory, String fileName){
+		File file = new File(directory);
+		int resultTotal = 0;
 		
-            //do you have permission to read this directory?	
-	    if (file.canRead()) {
-		for (File temp : file.listFiles()) {
-		    if (temp.isDirectory()) {
-			search(temp);
-		    } else {
-			if (getFileNameToSearch().equals(temp.getName().toLowerCase())) {			
-			    result.add(temp.getAbsoluteFile().toString());
-		    }
-
+		if(directory.endsWith(fileName)){
+			resultTotal++;
 		}
-	    }
-
-	 } else {
-		System.out.println(file.getAbsoluteFile() + "Permission Denied");
-	 }
-      }
-
-  }
-
+		
+		if(file.isDirectory()){
+			for(File nextPathLevel : file.listFiles()){
+				resultTotal += this.searchForFile(nextPathLevel.getAbsolutePath(), fileName);
+				
+				
+				/*iterative searching
+				 * if(nextPathLevel.getAbsolutePath().endsWith(fileName)){
+				 * resultTotal++;
+				 * }
+				 * */
+			}
+		}
+		return resultTotal;
+	}
+	
+	public static void main(String[] args){
+		BinaryFileSearch bfs = new BinaryFileSearch();
+		String directory = args[0];
+		String fileName = args[1];
+		int results = bfs.searchForFile(directory, fileName);
+		System.out.printf("Total of : " + results + "," + fileName);
+	}
 }
