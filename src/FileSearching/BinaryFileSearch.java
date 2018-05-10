701d545ccb136aa515d1ae854c9f920398250e85
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-09 20:48:40 -0700

    All Classes Done
    All comments and clean up jobs done. Methods tested and drivers tested with added functionality for  creating multiple instantiations.

--- src/FileSearching/BinaryFileSearch.java
+++ src/FileSearching/BinaryFileSearch.java
@@ -12,8 +12,21 @@ package FileSearching;
 /*						PSEUDOCODE					*/
 /* class BinaryFileSearch{
  * 
+ * method for searching takes two parameters for path
+ * and file name.
  * 
+ * create file object to get access to IO methods
  * 
+ * declare counter for counting results
+ * 
+ * check condition 1:
+ * 		condition 1: if the path is the same as the file name, 
+ * 		then file has been found, increase counter
+ * check condition 2 (if 1 is not satisfied):
+ * 		is the file object a directory, if not, pass
+ * 		current name of object as file name to method (recursive call)
+ * 		to check again. If condition satisfied, then file is found and
+ * 		increase counter.
  * }
  * */
 /****************************************************/
@@ -22,7 +35,15 @@ import java.io.File;
 
 public class BinaryFileSearch {
 
-	
+	/* Two arg method for searching for a file by name
+	 * contained in a String variable. This method will
+	 * check the directory and fileName String values
+	 * against the conditions within. Declaring a new File
+	 * Object gives access to methods such as isDirectory
+	 * that can check for path values. If the value is a directory,
+	 * then make the recursive call to the method to pass
+	 * the fileName value in again and run it against the conditions
+	 * until one is satisfied.*/
 	public int searchForFile(String directory, String fileName){
 		File file = new File(directory);
 		int resultTotal = 0;
@@ -31,9 +52,12 @@ public class BinaryFileSearch {
 			resultTotal++;
 		}
 		
+		//isDirectory() is a method of the File class
+		//that can check is the value of the object
+		//is a path.
 		if(file.isDirectory()){
 			for(File nextPathLevel : file.listFiles()){
-				resultTotal += this.searchForFile(nextPathLevel.getAbsolutePath(), fileName);
+				resultTotal += this.searchForFile(nextPathLevel.getAbsolutePath(), fileName); //recursive call
 				
 				
 				/*iterative searching
@@ -48,9 +72,9 @@ public class BinaryFileSearch {
 	
 	public static void main(String[] args){
 		BinaryFileSearch bfs = new BinaryFileSearch();
-		String directory = args[0];
-		String fileName = args[1];
+		String directory = "C:/Users/Quigy Laptop/workspace/";
+		String fileName = ".java";
 		int results = bfs.searchForFile(directory, fileName);
-		System.out.printf("Total of : " + results + "," + fileName);
+		System.out.printf("Total of : " + results + ", " + fileName);
 	}
 }
--- src/FractalFlake.java
+++ src/FractalFlake.java
@@ -20,7 +20,7 @@ public class FractalFlake extends Shape {
 	 * constructor for assignment. The values are obtained
 	 * by calling getX() or getY() from this class. 
 	 * The constructor below will also set the "size"
-	 * of the line to the private final int instance variable
+	 * of the line to the private instance variable
 	 * and the "numBranches" to the variable for the
 	 * number of "branches", or recursive passes that will
 	 * be made before the base case is reached
@@ -31,21 +31,33 @@ public class FractalFlake extends Shape {
 		this.limit = limit;
 	}
 
-		@Override
-		public void draw(Graphics g){
-			draw(g, getX(), getY(),size, limit);
+	/* This method is overridden by the method below and called
+	 * when the line is bring drawn for each recursive call.
+	 * */
+	@Override
+	public void draw(Graphics g){
+		draw(g, getX(), getY(),size, limit);
+	}
+
+	public void draw(Graphics g, int x, int y, int size, int limit) {
+		for (int i=0;i<360;i+=60) { //go in a complete circle
+			double rad = i * Math.PI / 180; //convert radians to degrees
+			/*using the cos() and sin() functions we can get 
+			 * the offset for the points to draw TO which should be 
+			 * the action of setting new x,y coordinates to set 
+			 * the line to draw TO.*/
+			final int x2 = (int) (x + Math.cos(rad) * size);
+			final int y2 = (int) (y + Math.sin(rad) * size);
+
+			if (limit >= 3) { //when recursive call takes in parameter value for "limit" and "limit" is less than or equal to 3
+				//then the base case has been reached and the calls stop.
+				g.drawLine(x, y, x2, y2);
+				/*will send these values up to the draw() method above that's overloaded
+				 * and only happens after a line is drawn. This ensures that when the line is drawn,
+				 * the next one is drawn from that last "suspended" position, and then only continues
+				 * to the next line when the base case is met. This happens until limit-1 <= 3*/
+				draw(g, x2, y2, size/3, limit-1);
+			}
 		}
-		
-		/**/
-		public void draw(Graphics g, int x, int y, int size, int limit) {
-		    for (int a = 0; a < 360; a += 60) { //go in a complete circle
-		        double rad = a * Math.PI / 180;
-		        int x2 = (int) (x + Math.cos(rad) * size);
-		        int y2 = (int) (y + Math.sin(rad) * size);
-		        if (limit >= 3) {
-		        	g.drawLine(x, y, x2, y2);
-		            draw(g, x2, y2, size/3, limit-1);
-		        }
-		    }
-		}
+	}
 }
